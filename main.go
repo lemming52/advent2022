75f55cc7e318f5be469325f5083024913ac9ca48
Andrew Strange <andrew@gumboot.co.uk>
2022-12-12 01:10:41 +0000

    d11: monkeyinthemiddle
    add the detailed simulation that doesn't work for large numbers

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"advent/solutions/d08treetoptreehouse"
 	"advent/solutions/d09ropebridge"
 	"advent/solutions/d10cathoderaytube"
+	"advent/solutions/d11monkeyinthemiddle"
 	"flag"
 	"fmt"
 	"time"
@@ -33,6 +34,7 @@ func main() {
 		"treetoptreehouse",
 		"ropebridge",
 		"cathoderaytube",
+		"monkeyinthemiddle",
 	}
 	if *all {
 		previous := time.Now()
@@ -51,46 +53,51 @@ func main() {
 func RunChallenge(challenge string) string {
 	var res string
 	switch challenge {
-	case "caloriecounting", "1", "01":
-		input := "inputs/d01caloriecounting.txt"
+	case "caloriecounting", "1":
+        input := "inputs/d01caloriecounting.txt"
 		A, B := d01caloriecounting.Run(input)
 		res = fmt.Sprintf("caloriecounting Results A: %s B: %s", A, B)
-	case "rockpaperscissors", "2", "02":
-		input := "inputs/d02rockpaperscissors.txt"
+	case "rockpaperscissors", "2":
+        input := "inputs/d02rockpaperscissors.txt"
 		A, B := d02rockpaperscissors.Run(input)
 		res = fmt.Sprintf("rockpaperscissors Results A: %s B: %s", A, B)
-	case "rucksackreorganisation", "3", "03":
-		input := "inputs/d03rucksackreorganisation.txt"
+	case "rucksackreorganisation", "3":
+        input := "inputs/d03rucksackreorganisation.txt"
 		A, B := d03rucksackreorganisation.Run(input)
 		res = fmt.Sprintf("rucksackreorganisation Results A: %s B: %s", A, B)
-	case "campcleanup", "4", "04":
-		input := "inputs/d04campcleanup.txt"
+	case "campcleanup", "4":
+        input := "inputs/d04campcleanup.txt"
 		A, B := d04campcleanup.Run(input)
 		res = fmt.Sprintf("campcleanup Results A: %s B: %s", A, B)
-	case "supplystacks", "5", "05":
-		input := "inputs/d05supplystacks.txt"
+	case "supplystacks", "5":
+        input := "inputs/d05supplystacks.txt"
 		A, B := d05supplystacks.Run(input)
 		res = fmt.Sprintf("supplystacks Results A: %s B: %s", A, B)
-	case "tuningtrouble", "6", "06":
-		input := "inputs/d06tuningtrouble.txt"
+	case "tuningtrouble", "6":
+        input := "inputs/d06tuningtrouble.txt"
 		A, B := d06tuningtrouble.Run(input)
 		res = fmt.Sprintf("tuningtrouble Results A: %s B: %s", A, B)
-	case "nospaceleftondevice", "7", "07":
-		input := "inputs/d07nospaceleftondevice.txt"
+	case "nospaceleftondevice", "7":
+        input := "inputs/d07nospaceleftondevice.txt"
 		A, B := d07nospaceleftondevice.Run(input)
 		res = fmt.Sprintf("nospaceleftondevice Results A: %s B: %s", A, B)
-	case "treetoptreehouse", "8", "08":
-		input := "inputs/d08treetoptreehouse.txt"
+	case "treetoptreehouse", "8":
+        input := "inputs/d08treetoptreehouse.txt"
 		A, B := d08treetoptreehouse.Run(input)
 		res = fmt.Sprintf("treetoptreehouse Results A: %s B: %s", A, B)
-	case "ropebridge", "9", "09":
-		input := "inputs/d09ropebridge.txt"
+	case "ropebridge", "9":
+        input := "inputs/d09ropebridge.txt"
 		A, B := d09ropebridge.Run(input)
 		res = fmt.Sprintf("ropebridge Results A: %s B: %s", A, B)
 	case "cathoderaytube", "10":
-		input := "inputs/d10cathoderaytube.txt"
+        input := "inputs/d10cathoderaytube.txt"
 		A, B := d10cathoderaytube.Run(input)
 		res = fmt.Sprintf("cathoderaytube Results A: %s B: %s", A, B)
-	}
+	case "monkeyinthemiddle", "11":
+        input := "inputs/d11monkeyinthemiddle.txt"
+		A, B := d11monkeyinthemiddle.Run(input)
+		res = fmt.Sprintf("monkeyinthemiddle Results A: %s B: %s", A, B)
+
+    }
 	return res
 }
---
+++ solutions/d11monkeyinthemiddle/monkeyinthemiddle.go
@@ -0,0 +1,167 @@
+package d11monkeyinthemiddle
+
+import (
+	"advent/solutions/utils"
+	"log"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const operationPattern = `  Operation: new = old ([+\-*\/]{1}) ([a-z0-9]+)`
+const targetPattern = `    If (true|false): throw to monkey (\d)+`
+const testPattern = `  Test: divisible by ([0-9]+)`
+
+type OperationFunction func(int) int
+type TestFunction func(int) bool
+
+type Monkey struct {
+	items         []int
+	name          int
+	operationFunc OperationFunction
+	testFunc      TestFunction
+	trueTarget    int
+	falseTarget   int
+	inspectCount  int
+}
+
+func (m *Monkey) inspect(targets map[int]*Monkey) {
+	for _, i := range m.items {
+		m.inspectCount += 1
+		worry := m.operationFunc(i)
+		worry = int(worry / 3)
+		target := 0
+		if m.testFunc(worry) {
+			target = m.trueTarget
+		} else {
+			target = m.falseTarget
+		}
+		t, ok := targets[target]
+		if !ok {
+			log.Fatal("no target", target)
+		}
+		t.items = append(t.items, worry)
+	}
+	m.items = []int{}
+}
+
+func ParseMonkey(input []string, number int) *Monkey {
+	monkey := &Monkey{
+		name:         number,
+		inspectCount: 0,
+	}
+	ParseItemLine(input[1], monkey)
+	ParseOperationLine(input[2], monkey)
+	ParseTestLines(input[3:6], monkey)
+	return monkey
+}
+
+func ParseItemLine(s string, m *Monkey) {
+	items := strings.Split(s, " ")[4:]
+	for _, i := range items {
+		val, err := strconv.Atoi(strings.Trim(i, ","))
+		if err != nil {
+			log.Fatal(err)
+		}
+		m.items = append(m.items, val)
+	}
+}
+
+func ParseOperationLine(s string, m *Monkey) {
+	pattern := regexp.MustCompile(operationPattern)
+	components := pattern.FindStringSubmatch(s)
+	useInput := components[2] == "old"
+	val := 0
+	var err error
+	if !useInput {
+		val, err = strconv.Atoi(components[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	switch components[1] {
+	case "+":
+		m.operationFunc = Addition(val, useInput)
+	case "*":
+		m.operationFunc = Multiplication(val, useInput)
+	case "-", "/":
+		panic("not implemented")
+	}
+}
+
+func Addition(factor int, useInput bool) OperationFunction {
+	if useInput {
+		return func(input int) int {
+			return input + input
+		}
+	} else {
+		return func(input int) int {
+			return input + factor
+		}
+	}
+}
+
+func Multiplication(factor int, useInput bool) OperationFunction {
+	if useInput {
+		return func(input int) int {
+			return input * input
+		}
+	} else {
+		return func(input int) int {
+			return input * factor
+		}
+	}
+}
+
+func ParseTestLines(lines []string, m *Monkey) {
+	testLine := lines[0]
+	trueTarget, falseTarget := lines[1], lines[2]
+	testP := regexp.MustCompile(testPattern)
+	targetP := regexp.MustCompile(targetPattern)
+	testComponents := testP.FindStringSubmatch(testLine)
+	divisibleVal, err := strconv.Atoi(testComponents[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	m.testFunc = Divisible(divisibleVal)
+	trueComponents := targetP.FindStringSubmatch(trueTarget)
+	trueVal, err := strconv.Atoi(trueComponents[2])
+	m.trueTarget = trueVal
+	falseComponents := targetP.FindStringSubmatch(falseTarget)
+	falseVal, err := strconv.Atoi(falseComponents[2])
+	m.falseTarget = falseVal
+}
+
+func Divisible(factor int) TestFunction {
+	return func(input int) bool {
+		return input%factor == 0
+	}
+}
+
+func MonkeyBusiness(input []string, rounds int) int {
+	monkeys := map[int]*Monkey{}
+	for i := 0; i <= len(input)/7; i++ {
+		monkeys[i] = ParseMonkey(input[i*7:], i)
+	}
+	Play(monkeys, rounds)
+	values := make([]int, len(monkeys))
+	for k, v := range monkeys {
+		values[k] = v.inspectCount
+	}
+	sort.Ints(values)
+	return values[len(values)-1] * values[len(values)-2]
+}
+
+func Play(monkeys map[int]*Monkey, rounds int) {
+	for i := 0; i < rounds; i++ {
+		for j := 0; j < len(monkeys); j++ {
+			monkeys[j].inspect(monkeys)
+		}
+	}
+}
+
+func Run(path string) (string, string) {
+	lines := utils.LoadAsStrings(path)
+	return strconv.Itoa(MonkeyBusiness(lines, 20)), "B"
+}
